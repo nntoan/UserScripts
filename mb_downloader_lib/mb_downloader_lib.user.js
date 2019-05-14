d1a712dcea916aeb2bb5e35329d64a932c9aca94
Toan Nguyen <nntoan@users.noreply.github.com>
2019-05-14 17:06:34 +0700

    Added MB Download Library

---
+++ mb_downloader_lib/mb_downloader_lib.user.js
@@ -0,0 +1,391 @@
+// ==UserScript==
+// @name         MB Downloader Library
+// @namespace    https://nntoan.com/
+// @description  Tải truyện từ các trang đọc truyện phổ biến dưới định dạng epub.
+// @version      0.0.1
+// @icon         https://i.imgur.com/1Wyz9je.jpg
+// @author       Toan Nguyen
+// @oujs:author  nntoan
+// @license      MIT; https://nntoan.mit-license.org/
+// @require      https://code.jquery.com/jquery-3.3.1.min.js
+// @require      https://unpkg.com/jepub@1.2.5/dist/jepub.min.js
+// @require      https://unpkg.com/file-saver@2.0.1/dist/FileSaver.min.js
+// @supportURL   https://github.com/nntoan/UserScripts/issues
+// @grant        unsafeWindow
+// @grant        GM_addStyle
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_xmlhttpRequest
+// @grant        GM_registerMenuCommand
+// @grant        GM_deleteValue
+// @grant        GM_listValues
+// @connect      *
+// @run-at       document-start
+// ==/UserScript==
+/*jshint evil:true newcap:false*/
+/*global unsafeWindow, GM_addStyle, GM_getValue, GM_setValue, GM_xmlhttpRequest, GM_registerMenuCommand, GM_deleteValue, GM_listValues, $, document, console, location, setInterval, setTimeout, clearInterval, jEpub, saveAs*/
+(function(window) {
+    "use strict";
+
+    var MbDownloader = {
+        jepub: null,
+        processing: {
+            count: 0,
+            begin: '',
+            end: '',
+            endDownload: false,
+            beginEnd: '',
+            titleError: [],
+            chapListSize: 0,
+        },
+        elements: {
+            $window: $(window),
+            $downloadBtn: $('<a>', {
+                class: 'btn blue btn-download',
+                href: 'javascript:;',
+                text: 'Tải xuống'
+            }),
+            $novelId: $(MbDownloader.options.classNames.novelId),
+            $infoBlock: $(MbDownloader.options.classNames.infoBlock),
+        },
+        options: {
+            errorAlert: true,
+            credits: '<p>UserScript được viết bởi: <a href="https://nntoan.com/">Toan Nguyen</a></p>',
+            general: {
+                host: location.host,
+                pathname: location.pathname,
+                referrer: location.protocol + '//' + MbDownloader.options.general.host + MbDownloader.options.general.pathname,
+                pageName: document.title,
+            },
+            processing: {
+                ebookFileName: null,
+                ebookFileExt: '.epub',
+                documentTitle: '[...] Vui lòng chờ trong giây lát',
+            },
+            regularExp: {
+                chapter: ['\s*Chương\s*\d+\s?:.*[^<\n]', 'g'],
+                novel: ['\s*Tiểu\s*thuyết\s?:.*[^<\n]', 'g'],
+                chineseSpecialChars: ['[^\x09\x0A\x0D\x20-\uD7FF\uE000-\uFFFD\u10000-\u10FFFF]+', 'gm'],
+                alphanumeric: ['\s[a-zA-Z0-9]{6,8}(="")?\s', 'gm'],
+                alphabet: ['[A-Z]'],
+                number: ['\d+'],
+                buttons: ['\([^(]+<button[^/]+<\/button>[^)]*\)\s*', 'gi'],
+                eoctext: ['(ps:|hoan nghênh quảng đại bạn đọc quang lâm|Huyền ảo khoái trí ân cừu)', 'i'],
+                breakline: ['\n', 'g'],
+                chapList: ['(?:href=")[^")]+(?=")', 'g'],
+            },
+            classNames: {
+                novelId: null,
+                infoBlock: null,
+                chapterContent: null,
+                chapterNotContent: null,
+                chapterVip: '#btnChapterVip',
+                ebookTitle: null,
+                ebookAuthor: null,
+                ebookCover: null,
+                ebookDesc: null,
+                ebookType: null,
+                downloadBtnStatus: 'btn-primary btn-success btn-info btn-warning btn-danger blue success warning info danger error',
+                downloadAppendTo: null,
+            },
+            ebook: {
+                title: null,
+                author: null,
+                description: null,
+                tags: null,
+                publisher: MbDownloader.options.general.host,
+            },
+            chapters: {
+                chapList: [],
+                chapListSlice: [6, -1],
+                chapId: null,
+                chapTitle: null,
+            },
+            xhr: {
+                chapter: {
+                    type: 'GET',
+                    url: null,
+                    data: {},
+                },
+                content: {
+                    type: 'GET',
+                    url: MbDownloader.options.general.pathname + MbDownloader.options.chapters.chapId + '/',
+                    xhrFields: {
+                        withCredentials: true
+                    }
+                }
+            }
+        },
+
+        /**
+         * Initialise MB Downloader
+         */
+        init: function () {
+            if (!this.elements.$novelId.length) {
+                return;
+            }
+
+            // Prepare & register jEpub instance
+            this.getBookInfo();
+            this.jepub = jEpub(this.options.ebook);
+
+            // Works with download button
+            this.elements.$downloadBtn.appendTo(this.options.classNames.downloadAppendTo);
+            this.registerEventHandlers(this.elements.$downloadBtn, 'dl');
+        },
+
+        getBookInfo: function () {
+            var options = this.options;
+
+            options.ebook.title = this.elements.$infoBlock.find(options.classNames.ebookTitle).text().trim();
+            options.ebook.author = this.elements.$infoBlock.find(options.classNames.ebookAuthor).find('p').text().trim();
+            options.ebook.cover = this.elements.$infoBlock.find(options.classNames.ebookCover).find('img').attr('src');
+            options.ebook.description= this.elements.$infoBlock.find(options.classNames.ebookDesc).html();
+            var $ebookType = this.elements.$infoBlock.find(options.classNames.ebookType);
+            if ($ebookType.length) {
+                $ebookType.each(function () {
+                    options.ebook.tags.push($(this).text().trim());
+                });
+            }
+        },
+
+        createRegExp: function (regExp) {
+            if (!regExp.length) {
+                return;
+            }
+
+            return new RegExp(regExp[0], regExp[1]);
+        },
+
+        registerEventHandlers: function ($widget, typeFn) {
+            var self = this,
+                options = this.options;
+
+            if (typeFn === 'dl') {
+                $widget.one('click contextmenu', function (e) {
+                    e.preventDefault();
+
+                    document.title = options.processing.documentTitle;
+
+                    $.ajax(options.xhr.chapter).done(function (response) {
+                        options.chapters.chapList = response.match(self.createRegExp(options.regularExp.chapList));
+                        options.chapters.chapList = options.chapters.chapList.map(self.chapListValueFilter);
+
+                        if (e.type === 'contextmenu') {
+                            $widget.off('click');
+                            var startFrom = prompt('Nhập ID chương truyện bắt đầu tải:', options.chapters.chapList[0]);
+                            startFrom = options.chapters.chapList.indexOf(startFrom);
+                            if (startFrom !== -1) {
+                                options.chapters.chapList = options.chapters.chapList.slice(startFrom);
+                            }
+                        } else {
+                            $widget.off('contextmenu');
+                        }
+
+                        self.processing.chapListSize = options.chapters.chapList.length;
+                        if (self.processing.chapListSize > 0) {
+                            self.elements.$window.on('beforeunload', function () {
+                                return 'Truyện đang được tải xuống...';
+                            });
+
+                            $widget.one('click', function (e) {
+                                e.preventDefault();
+                                self.saveEbook();
+                            });
+
+                            self.getContent($widget);
+                        }
+                    }).fail(function (error) {
+                        $widget.text('Lỗi danh mục');
+                        self.downloadStatus('error');
+                        console.error(error);
+                    });
+                });
+            }
+        },
+
+        getContent: function ($widget) {
+            var self = this,
+                options = this.options;
+
+            if (self.processing.endDownload === true) {
+                return;
+            }
+
+            options.chapters.chapId = options.chapters.chapList[self.processing.count];
+
+            $.ajax(options.xhr.content).done(function (response) {
+                var $data = $(response),
+                    $chapter = $data.find(options.classNames.chapterContent),
+                    $notContent = $chapter.find(options.classNames.chapterNotContent),
+                    $referrer = $chapter.find('[style]').filter(function () {
+                        return (this.style.fontSize === '1px' || this.style.fontSize === '0px' || this.style.color === 'white');
+                    }),
+                    chapContent;
+
+                if (self.processing.endDownload === true) {
+                    return;
+                }
+
+                options.chapters.chapTitle = $data.find('h2').text().trim();
+                if (options.chapters.chapTitle === '') {
+                    options.chapters.chapTitle = 'Chương ' + options.chapters.chapId.match(self.createRegExp(options.regularExp.number))[0];
+                }
+
+                if (!$chapter.length) {
+                    chapContent = self.downloadError('Không có nội dung');
+                } else {
+                    if ($chapter.find(options.classNames.chapterVip).length) {
+                        chapContent = self.downloadError('Chương VIP');
+                    } else if ($chapter.filter(function () {
+                        return (this.textContent.toLowerCase().indexOf('vui lòng đăng nhập để đọc chương này') !== -1);
+                    }).length) {
+                        chapContent = self.downloadError('Chương yêu cầu đăng nhập');
+                    } else {
+                        var $img = $chapter.find('img');
+                        if ($img.length) {
+                            $img.replaceWith(function () {
+                                return '<br /><a href="' + this.src + '">Click để xem ảnh</a><br />';
+                            });
+                        }
+
+                        if ($notContent.length) $notContent.remove();
+                        if ($referrer.length) $referrer.remove();
+
+                        if ($chapter.text().trim() === '') {
+                            chapContent = self.downloadError('Nội dung không có');
+                        } else {
+                            if (!$widget.hasClass('error')) {
+                                self.downloadStatus('warning');
+                            }
+                            chapContent = self.parseHtml($chapter.html());
+                        }
+                    }
+                }
+
+                self.jepub.add(options.chapters.chapTitle, chapContent);
+
+                if (self.processing.count === 0) {
+                    self.processing.begin = options.chapters.chapTitle;
+                }
+                self.processing.end = options.chapters.chapTitle;
+
+                $widget.html('Đang tải: ' + Math.floor((self.processing.count / self.processing.chapListSize) * 100) + '%');
+
+                self.processing.count++;
+                document.title = '[' + self.processing.count + '] ' + options.general.pageName;
+                if (self.processing.count >= self.processing.chapListSize) {
+                    self.saveEbook($widget);
+                } else {
+                    self.getContent($widget);
+                }
+            }).fail(function (error) {
+                self.downloadError('Kết nối không ổn định', error);
+                self.saveEbook($widget);
+            });
+        },
+
+        chapListValueFilter: function (val) {
+            var options = this.options;
+
+            val = val.slice(options.chapters.chapListSlice[0], options.chapters.chapListSlice[1]);
+            val = val.replace(options.general.referrer, '');
+
+            return val.trim();
+        },
+
+        downloadStatus: function (status) {
+            var self = this,
+                options = this.options;
+
+            self.elements.$downloadBtn.removeClass(options.classNames.downloadBtnStatus).addClass('btn-' + status).addClass(status);
+        },
+
+        downloadError: function(error, message) {
+            var options = this.options;
+
+            this.downloadStatus('error');
+            this.processing.titleError.push(options.chapters.chapTitle);
+            if (options.errorAlert) {
+                options.errorAlert = confirm('Lỗi! ' + message + '\nBạn có muốn tiếp tục nhận cảnh báo?');
+            }
+
+            if (error) {
+                console.error(message);
+            }
+
+            return '<p class="no-indent"><a href="' + options.general.referrer + options.chapters.chapId + '">' + message + '</a></p>';
+        },
+
+        parseHtml: function (html) {
+            var options = this.options;
+
+            html = html.replace(this.createRegExp(options.regularExp.chapter), '');
+            html = html.replace(this.createRegExp(options.regularExp.novel), '');
+            html = html.replace(this.createRegExp(options.regularExp.chineseSpecialChars), ''); // eslint-disable-line
+            html = html.replace(this.createRegExp(options.regularExp.alphanumeric), function (key, attr) {
+                if (attr) return ' ';
+                if (!isNaN(key)) return key;
+                if (key.split(this.createRegExp(options.regularExp.alphabet)).length > 2) return ' ';
+                if (key.split(this.createRegExp(options.regularExp.number)).length > 1) return ' ';
+                return key;
+            });
+            html = html.replace(this.createRegExp(options.regularExp.buttons), '');
+            html = html.split(this.createRegExp(options.regularExp.eoctext))[0];
+            html = html.replace(this.createRegExp(options.regularExp.breakline), '<br />');
+            return '<div>' + html + '</div>';
+        },
+
+        saveEbook: function ($widget) {
+            var self = this,
+                options = this.options;
+
+            if (self.processing.endDownload) {
+                return;
+            }
+
+            self.processing.endDownload = true;
+            $widget.html('Đang nén EPUB...');
+
+            if (options.chapters.chapTitle.length) {
+                self.processing.titleError = '<p class="no-indent"><strong>Các chương lỗi: </strong>' + self.processing.titleError.join(', ') + '</p>';
+            } else {
+                self.processing.titleError = '';
+            }
+            self.processing.beginEnd = '<p class="no-indent">Nội dung từ <strong>' + self.processing.begin + '</strong> đến <strong>' + self.processing.end + '</strong></p>';
+
+            self.jepub.notes(self.processing.beginEnd + self.processing.titleError + '<br /><br />' + options.credits);
+
+            self.jepub.generate().then(self.afterGenerateEpub).catch(function (error) {
+                self.downloadStatus('error');
+                console.error(error);
+            });
+        },
+
+        afterGenerateEpub: function (epubZipContent) {
+            var self = this,
+                options = this.options,
+                ebookFilepath = options.processing.ebookFilename + options.processing.ebookFileExt;
+
+            document.title = '[⇓] ' + options.ebook.title;
+            self.elements.$window.off('beforeunload');
+
+            self.elements.$downloadBtn.attr({
+                href: window.URL.createObjectURL(epubZipContent),
+                download: ebookFilepath
+            }).text('✓ Hoàn thành').off('click');
+            if (!self.elements.$downloadBtn.hasClass('error')) {
+                self.downloadStatus('success');
+            }
+
+            saveAs(epubZipContent, ebookFilepath);
+        },
+    };
+
+    // assign MbDownloader to the window object
+    if (typeof window.MbDownloader !== "object") {
+        // if it doesn't exist, just add it
+        window.MbDownloader = MbDownloader;
+        window._Mbd = MbDownloader;
+    }
+})(window);
